2e8db8ccd41c38de813a46cfe8944fa8cdd5a59b
Pierrick Aubin <pierrick.aubin@siqual.fr>
2016-10-07 16:41:37 +0200

    Add functions

--- Service/GoogleCalendar.php
+++ Service/GoogleCalendar.php
@@ -119,6 +119,114 @@ class GoogleCalendar
     }
 
     /**
+     * Add an Event to the specified calendar
+     *
+     * @param string $calendarId Calendar's ID in which you want to insert your event
+     * @param \DateTime $eventStart Event's start date
+     * @param \DateTime $eventEnd Event's end date
+     * @param string $eventSummary Event's title
+     * @param string $eventDescription Event's description where you should put all your informations
+     * @param array $eventAttendee Event's attendees : to use the invitation system you should add the calendar owner to the attendees
+     * @param array $optionalParams Optional params
+     *
+     * @return object Event
+     */
+    public function addEvent(
+        $calendarId,
+        $eventStart,
+        $eventEnd,
+        $eventSummary,
+        $eventDescription,
+        $eventAttendee,
+        $optionalParams = []
+    )
+    {
+        // Your new GoogleEvent object
+        $event = new \Google_Service_Calendar_Event();
+        // Set the title
+        $event->setSummary($eventSummary);
+        // Set and format the start date
+        $formattedStart = $eventStart->format(\DateTime::RFC3339);
+        $formattedEnd = $eventEnd->format(\DateTime::RFC3339);
+        $start = new \Google_Service_Calendar_EventDateTime();
+        $start->setDateTime($formattedStart);
+        $event->setStart($start);
+        $end = new \Google_Service_Calendar_EventDateTime();
+        $end->setDateTime($formattedEnd);
+        $event->setEnd($end);
+        // Default status for newly created event
+        $event->setStatus('tentative');
+        // Set event's description
+        $event->setDescription($eventDescription);
+        // Attendees - permit to manage the event's status
+        $attendee = new \Google_Service_Calendar_EventAttendee();
+        $attendee->setEmail($eventAttendee);
+        $event->attendees = [$attendee];
+        // Event insert
+        return $this->getCalendarService()->events->insert($calendarId, $event, $optionalParams);
+    }
+
+    /**
+     * Retrieve modified events from a Google push notification
+     *
+     * @param string $calendarId
+     * @param string $syncToken Synchronised Token to retrieve last changes
+     *
+     * @return object
+     */
+    public function getEvents($calendarId, $syncToken)
+    {
+        // Option array
+        $optParams = [];
+        return $this->getCalendarService()->events->listEvents($calendarId, $optParams);
+    }
+
+    /**
+     * Init a full list of events
+     *
+     * @param string $calendarId
+     *
+     * @return object
+     */
+    public function initEventsList($calendarId)
+    {
+        $eventsList = $this->getCalendarService()->events->listEvents($calendarId);
+        return $eventsList->getItems();
+    }
+
+    /**
+     * Delete an event
+     *
+     * @param string $calendarId
+     * @param string $eventId
+     */
+    public function deleteEvent($calendarId, $eventId)
+    {
+        $this->getCalendarService()->events->delete($calendarId, $eventId);
+    }
+
+    /**
+     * Update an event
+     *
+     * @param string $calendarId
+     * @param \Google_Service_Calendar_Event $event
+     */
+    public function updateEvent($calendarId, $event)
+    {
+        $this->getCalendarService()->events->update($calendarId, $event->getId(), $event);
+    }
+
+    /**
+     * List shared and available calendars
+     *
+     * @return object
+     */
+    public function listCalendars()
+    {
+        return $this->getCalendarService()->calendarList->listCalendarList();
+    }
+
+    /**
      * Retrieve Google events on a date range
      *
      * @param string $calendarId
@@ -138,6 +246,24 @@ class GoogleCalendar
             'timeMin' => $timeMin,
             'timeMax' => $timeMax
         );
+        $eventList = $service->events->listEvents($calendarId, $eventOptions);
+        return $eventList;
+    }
+
+    /**
+     * Retrieve Google events filtered by parameters
+     *
+     * @param string $calendarId
+     * @param array $eventOptions
+     *
+     * @return object
+     */
+    public function getEventsByParams($calendarId, $eventOptions)
+    {
+        $service = $this->getCalendarService();
+        foreach (['timeMin', 'timeMax', 'updatedMin'] as $opt) {
+            if (isset($eventOptions[$opt])) $eventOptions[$opt] = $eventOptions[$opt]->format(\DateTime::RFC3339);
+        }
         $eventList = $service->events->listEvents($calendarId, $eventOptions);
         return $eventList;
     }
