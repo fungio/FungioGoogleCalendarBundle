9129d64b091c92016fe611d393a2d7f073bbfe51
Pierrick Aubin <pierrick.aubin@siqual.fr>
2016-11-14 17:49:04 +0100

    Add contact list support

--- Service/GoogleCalendar.php
+++ Service/GoogleCalendar.php
@@ -60,7 +60,7 @@ class GoogleCalendar
      */
     public function __construct()
     {
-        $this->scopes = implode(' ', [\Google_Service_Calendar::CALENDAR]);
+        $this->scopes = implode(' ', [\Google_Service_Calendar::CALENDAR, \Google_Service_Script::WWW_GOOGLE_COM_M8_FEEDS]);
     }
 
     /**
@@ -386,6 +386,30 @@ class GoogleCalendar
     }
 
     /**
+     * @param int $maxResults
+     * @return array
+     */
+    public function listContacts($maxResults = 200)
+    {
+        $accessToken = json_decode($this->getAccessToken(), 1)['access_token'];
+        $url = 'https://www.google.com/m8/feeds/contacts/default/full?max-results='.$maxResults.'&alt=json&v=3.0&oauth_token='.$accessToken;
+        $xmlresponse =  $this->curl($url);
+        $contacts = json_decode($xmlresponse,true);
+
+        $return = [];
+        if (!empty($contacts['feed']['entry'])) {
+            foreach($contacts['feed']['entry'] as $contact) {
+                //retrieve Name and email address
+                $return[] = [
+                    'name'=> $contact['title']['$t'],
+                    'email' => $contact['gd$email'][0]['address'],
+                ];
+            }
+        }
+        return $return;
+    }
+
+    /**
      * List shared and available calendars
      *
      * @return object
@@ -480,4 +504,39 @@ class GoogleCalendar
         }
         return null;
     }
+
+    /**
+     * @param $url
+     * @param string $post
+     * @return mixed
+     */
+    public function curl($url, $post = "")
+    {
+        $curl = curl_init();
+        $userAgent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)';
+        curl_setopt($curl, CURLOPT_URL, $url);
+        //The URL to fetch. This can also be set when initializing a session with curl_init().
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        //TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        //The number of seconds to wait while trying to connect.
+        if ($post != "") {
+            curl_setopt($curl, CURLOPT_POST, 5);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+        }
+        curl_setopt($curl, CURLOPT_USERAGENT, $userAgent);
+        //The contents of the "User-Agent: " header to be used in a HTTP request.
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+        //To follow any "Location: " header that the server sends as part of the HTTP header.
+        curl_setopt($curl, CURLOPT_AUTOREFERER, TRUE);
+        //To automatically set the Referer: field in requests where it follows a Location: redirect.
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        //The maximum number of seconds to allow cURL functions to execute.
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        //To stop cURL from verifying the peer's certificate.
+        $contents = curl_exec($curl);
+        curl_close($curl);
+        return $contents;
+    }
+
 }
